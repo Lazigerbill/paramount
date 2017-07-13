e08913ebd0f2dd1f6265f30831a2550d8f34efd2
Lazigerbill <chaukbill@gmail.com>
2017-07-13 12:50:23 -0400

    backend connected to aws-iot

--- client/startup/default.js
+++ client/startup/default.js
@@ -1,4 +1,10 @@
 // Run this when the meteor app is started
 Meteor.startup(function () {
 
+	console.log('client fired');
+	import AWS from 'aws-sdk';
+	import AWSIoTData from 'aws-iot-device-sdk';
+	// var AWSConfiguration = require('./aws-configuration.js');
+
+	console.log('Loaded AWS SDK for JavaScript and AWS IoT SDK for Node.js');
 });
