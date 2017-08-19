07472b33ac83c8dc775252e1baf179f53570adc3
Lazigerbill <chaukbill@gmail.com>
2017-08-19 01:43:11 -0400

    cleaned up device status card

--- imports/ui/components/dashboard/deviceStatus.js
+++ imports/ui/components/dashboard/deviceStatus.js
@@ -1,6 +1,20 @@
 import "./deviceStatus.html";
+import { ShadowDocs } from '/imports/api/shadowDocs.js';
+
+Template.deviceStatus.onCreated(function(){
+	this.subscribe("shadowDocs",{
+		onStop: function(e){
+			console.log(e);
+		}
+	})
+})
 
 Template.deviceStatus.helpers({
+	connect(){
+		const doc = ShadowDocs.find({}, {sort: {timestamp: -1}, limit:1}).fetch();
+		console.log(doc);
+		return doc[0].current.state.reported.connected
+	},
 	battery(){
 		return Session.get('battery');
 	},
